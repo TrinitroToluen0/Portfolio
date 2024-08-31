8441808bf25e8490be10d793ffee650792c00c88
TrinitroToluen0 <javiermenco404@gmail.com>
2024-08-30 21:48:29 -0500

    Update linkedin profile URL

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export default {
     CONTACT_ENDPOINT: "https://formsubmit.co/01f5f293c73f02a6346a169ebd8f9b0e",
-    LINKEDIN_PROFILE: "https://www.linkedin.com/in/javier-menco-211803325/",
+    LINKEDIN_PROFILE: "https://www.linkedin.com/in/javier-menco",
     GITHUB_PROFILE: "https://github.com/TrinitroToluen0",
     RECAPTCHA_SITE_KEY: "6LcUff8oAAAAAPtdKUjmSMbJs0b0j5GLz9bi_Swb",
 };
