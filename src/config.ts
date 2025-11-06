ec27ec099049df8a20ae105e17be54e8e02c75f1
TrinitroToluen0 <javiermenco404@gmail.com>
2025-11-05 20:08:28 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export default {
-    CONTACT_ENDPOINT: "https://api.javiermenco.com/portfolio/contact",
+    CONTACT_ENDPOINT: "https://portfolio-2av.pages.dev/contact",
     LINKEDIN_PROFILE: "https://www.linkedin.com/in/javier-menco",
     GITHUB_PROFILE: "https://github.com/TrinitroToluen0",
     RECAPTCHA_SITE_KEY: "6LcJsQMsAAAAAAZ5v94vOYleTr6Ee-g9sr_M8Gpl",
